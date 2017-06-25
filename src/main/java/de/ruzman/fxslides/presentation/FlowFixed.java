eee3b716acaef83d7c5d52e031772be307595813
ruzmanz <zoltan@ruzman.de>
2017-06-25 11:57:54 +0200

    Use FlowHandler to trigger actions on the current Flow

--- src/main/java/de/ruzman/fxslides/presentation/FlowFixed.java
+++ src/main/java/de/ruzman/fxslides/presentation/FlowFixed.java
@@ -6,7 +6,8 @@ import io.datafx.controller.flow.FlowHandler;
 import io.datafx.controller.flow.context.ViewFlowContext;
 
 public class FlowFixed extends Flow {
-
+	private FlowHandler flowHandler;
+	
 	public FlowFixed(Class<?> startViewControllerClass) {
 		this(startViewControllerClass, new ViewConfiguration());
 	}
@@ -16,10 +17,20 @@ public class FlowFixed extends Flow {
 	}
 	
     /**
+     * FIXED: Use FlowHandler to trigger actions on the current Flow.
+     * FIXED: Pass the globalViewConfiguration.
      * @see Flow#createHandler(ViewFlowContext)
      */
 	@Override
     public FlowHandler createHandler(ViewFlowContext flowContext) {
-        return new FlowHandler(this, flowContext, getViewConfiguration());
+		this.flowHandler = new FlowHandler(this, flowContext, getViewConfiguration());
+        return flowHandler;
     }
+	
+	/**
+	 * FIXED: Use FlowHandler to trigger actions on the current Flow.
+	 */
+	public FlowHandler getFlowHandler() {
+		return flowHandler;
+	}
 }
